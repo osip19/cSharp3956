f2c74e13370d5b02fcabc58e3308ad8cfb311687
Ivan L <ivan.linzov@gmail.com>
2023-02-24 22:14:37 +0300

    task 37 realized

---
+++ seminar5/task37/Program.cs
@@ -0,0 +1,39 @@
+﻿int[] FillArray(int length, int leftnum = int.MinValue, int rightnum = int.MaxValue)
+{
+    int[] rand_arr = new int[length];
+    for (int i = 0; i < rand_arr.Length; i++)
+    {
+        rand_arr[i] = Random.Shared.Next(leftnum, rightnum);
+    }
+    return rand_arr;
+}
+
+int[] LengthArray(int[] array)
+{
+    if(array.Length % 2 == 0) 
+    {
+        return new int[array.Length / 2];
+    }
+    else
+    {
+        return new int[array.Length / 2 + 1];
+    }
+}
+
+int[] MultiplyArray(int[] array)
+{
+    int[] result = LengthArray(array);
+    for(int i = 0; i < array.Length / 2; i++)
+    {
+        result[i] = array[i] * array[array.Length - i -1];
+    }
+    if(array.Length % 2 != 0)
+    {
+        result[result.Length - 1] = array[array.Length / 2];
+    }
+    return result;
+}
+
+int[] filled_array = FillArray(5, -10, 10);
+
+Console.WriteLine($"{string.Join(',', filled_array)} -> {string.Join(',', MultiplyArray(filled_array))}");
